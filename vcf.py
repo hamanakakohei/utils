e52e0c0b13e9044956bf42aef64384be646eea13
hamanakakohei <46100957+hamanakakohei@users.noreply.github.com>
2025-07-31 21:47:35 +0900

    Update vcf.py

--- vcf.py
+++ vcf.py
@@ -17,3 +17,11 @@ def read_vcf_as_df(vcf_path: Union[str, Path]) -> pd.DataFrame:
                 break
 
     return vcf_df
+
+
+def read_vcf_as_df_4_alphagenome(vcf_path: Union[str, Path], genome_build = 'b38') -> pd.DataFrame:
+    vcf_df = read_vcf_as_df(vcf_path)
+    vcf_df["variant_id"] = vcf_df.apply(
+        lambda row: f"{row['CHROM']}_{row['POS']}_{row['REF']}_{row['ALT']}_{genome_build}", axis=1
+    )
+    return vcf_df[["variant_id", "CHROM", "POS", "REF", "ALT"]]
