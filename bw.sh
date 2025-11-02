9b1cc8dc101d56a600a3ace8d6687a7e6007fae7
hamanakakohei <46100957+hamanakakohei@users.noreply.github.com>
2025-11-02 19:59:10 +0900

    Update bw.sh

--- bw.sh
+++ bw.sh
@@ -4,17 +4,51 @@ merge_bws(){
   BW_LIST=$1
   REF=$2
   GENOME_SIZES=$3
-  PREFIX=$4
-  APPTAINER=/usr/local/biotools/j/jvarkit:2024.08.25--hdfd78af_2
-
-  singularity exec $APPTAINER bash -c "
+  INTERVAL=$4 # chr1:234-567
+  PREFIX=$5
+  APP1=/usr/local/biotools/j/jvarkit:2024.08.25--hdfd78af_2
+  APP2=/usr/local/biotools/u/ucsc-bedgraphtobigwig:482--hdc0a859_0
+  
+  singularity exec $APP1 bash -c "
     export JAVA_HOME=/usr/local
     bigwigmerge \
       $BW_LIST \
       -R $REF \
       -m average \
+      -r $INTERVAL \
       -o ${PREFIX}.bg
   "
 
-  bedGraphToBigWig ${PREFIX}.bg $GENOME_SIZES ${PREFIX}.bw
+  singularity exec $APP2 bash -c "
+    bedGraphToBigWig \
+      ${PREFIX}.bg \
+      $GENOME_SIZES \
+      ${PREFIX}.bw  
+  "
+}
+
+subset_bw(){
+  PREFIX=$1
+  CHR=$2
+  STA=$3
+  END=$4
+  GENOME_SIZES=$5
+  APP1=/usr/local/biotools/u/ucsc-bigwigtobedgraph:482--h0b57e2e_0
+  APP2=/usr/local/biotools/u/ucsc-bedgraphtobigwig:482--hdc0a859_0
+
+  singularity exec $APP1 bash -c "
+    bigWigToBedGraph \
+      ${PREFIX}.bw \
+      ${PREFIX}.region.bg \
+      -chrom=$CHR \
+      -start=$STA \
+      -end=$END
+  "
+  
+  singularity exec $APP2 bash -c "
+    bedGraphToBigWig \
+      ${PREFIX}.region.bg \
+      $GENOME_SIZES \
+      ${PREFIX}.region.bw  
+  "
 }
